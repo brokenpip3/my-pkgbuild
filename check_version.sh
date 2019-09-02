58079afc2c4f8020c1c89750e99e29d1dffd9ebf
brokenpip3 <brokenpip3@gmail.com>
2019-09-02 13:52:17 +0200

    insert exit status code

--- check_version.sh
+++ check_version.sh
@@ -6,8 +6,8 @@ get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'|head -n 1   
 }
 
-pkgbuild="$1"
-source $HOME/$pkgbuild/PKGBUILD 
+package="$1"
+source $HOME/pkgbuild/$package/PKGBUILD 
 
 re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)$"
 if [[ $url =~ $re ]]; then
@@ -29,7 +29,7 @@ echo "####"
 if [[ ${lastver_clean} != "" ]] && [[ ${lastver_clean} != ${pkgver} ]]; then
   echo "Found version ${lastver_clean} different from ${pkgver}"
   echo "Let's try pkgbuild with version ${lastver}"
-  sed -i "s/pkgver=${pkgver}/pkgver=${lastver_clean}/g" "$HOME/$pkgbuild/PKGBUILD"
+  sed -i "s/pkgver=${pkgver}/pkgver=${lastver_clean}/g" "$HOME/pkgbuild/$package/PKGBUILD"
 else
   echo "No version tagged or no new version"
 fi
