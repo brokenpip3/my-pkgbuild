06e2043c0a04a694b321ceed2379d9865c4efb00
brokenpip3 <brokenpip3@gmail.com>
2019-09-02 13:30:12 +0200

    different stage

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#set -x
+
+send_tg_message() { 
+/usr/bin/curl --silent --output /dev/null --data-urlencode "chat_id=${TELEGRAM_CHAT_ID}" --data-urlencode "text=$1" \
+		--data-urlencode "parse_mode=markdown" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" 
+}
+
+prettyprint_up () {
+echo ""
+echo ""
+echo "####"
+}
+
+prettyprint_down () {
+echo "####"
+echo ""
+echo ""
+}
+
+RESULT=""
+package="$1"
+
+cd $HOME/$package/ 
+source PKGBUILD
+prettyprint_up 
+echo "Makepkg for pkg: $pkgname"
+prettyprint_down 
+makepkg -sci --noconfirm  --skipchecksums
+if [[ $? = 0 ]]; then
+prettyprint_up 
+echo "$pkgname success!"
+prettyprint_down 
+RESULT="$RESULT \n $pkgname - $pkgver - OK"
+else
+prettyprint_up 
+echo "$pkgname failed!"
+prettyprint_down 
+RESULT="$RESULT \n $pkgname - $pkgver - FAIL"
+fi
+
+printf ${RESULT}
+#send_tg_message ${RESULT}
---
+++ check_version.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#set -x
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'|head -n 1   
+}
+
+pkgbuild="$1"
+source $HOME/$pkgbuild/PKGBUILD 
+
+re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+)$"
+if [[ $url =~ $re ]]; then
+  protocol=${BASH_REMATCH[1]}
+  separator=${BASH_REMATCH[2]}
+  hostname=${BASH_REMATCH[3]}
+  user=${BASH_REMATCH[4]}
+  reponame=${BASH_REMATCH[5]}
+fi
+
+repo="$user/$reponame"
+lastver=$(get_latest_release "$repo")
+lastver_clean=$(grep -P -o '(\d(\.(?=\d))?){2,}' <<<"${lastver}")
+
+echo "####"
+echo "Check new version for pkgbuild: $pkgname"
+echo "####"
+
+if [[ ${lastver_clean} != "" ]] && [[ ${lastver_clean} != ${pkgver} ]]; then
+  echo "Found version ${lastver_clean} different from ${pkgver}"
+  echo "Let's try pkgbuild with version ${lastver}"
+  sed -i "s/pkgver=${pkgver}/pkgver=${lastver_clean}/g" "$HOME/$pkgbuild/PKGBUILD"
+else
+  echo "No version tagged or no new version"
+fi
